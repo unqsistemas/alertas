20e6251f716e7bc96e47946eb65abcd9136d4c1d
Hernan Slavich <hernan.slavich@unq.edu.ar>
2015-02-05 14:46:06 -0300

    Archivo de configuracion

--- src/app.php
+++ src/app.php
@@ -6,38 +6,32 @@ use Silex\Provider\RoutingServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\DoctrineServiceProvider;
+use Symfony\Component\Yaml\Yaml;
+
+$config = Yaml::parse(file_get_contents(__DIR__.'/../config/config.yml'));
 
 $app = new Application();
 $app->register(new RoutingServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
 $app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
-
-$app->register(new DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver'        => 'pdo_mysql',
-        'host'          => 'localhost',
-        'dbname'        => 'alertas',
-        'user'          => 'root',
-        'password'      => 'root',
-    )
-));
+$app->register(new DoctrineServiceProvider(), array('db.options' => $config['database']));
 
 $app->register(new Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider, array(
-    "orm.proxies_dir" => __DIR__.'/../var/cache/doctrine/',
-    "orm.em.options" => array(
-        "mappings" => array(
-            array(
-                "type" => "annotation",
-                "namespace" => "Entity",
-                "path" => __DIR__."/Entity"
-            )
-        )
-    )
+	"orm.proxies_dir" => __DIR__.'/../var/cache/doctrine/',
+	"orm.em.options" => array(
+		"mappings" => array(
+			array(
+				"type" => "annotation",
+				"namespace" => "Entity",
+				"path" => __DIR__."/Entity"
+			)
+		)
+	)
 ));
 
 $app['alerta_manager'] = function ($app) {
-    return new Alerta\AlertaManager($app);
+	return new Alerta\AlertaManager($app);
 };
 
 return $app;
